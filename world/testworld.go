61ed41c37b2913f10b157d7550943420fd1cbc2c
igta0222 <igor.tatarinov@netcracker.com>
2024-03-22 20:27:29 +0300

    BUC-1

--- world/testworld.go
+++ world/testworld.go
@@ -3,22 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"image/color"
 	"log"
 )
 
-type Game struct{}
+type Game struct {
+	images  []*ebiten.Image
+	options []*ebiten.DrawImageOptions
+}
 
 func (g *Game) Update() error {
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		fmt.Println("Test")
+		mouseX, mouseY := ebiten.CursorPosition()
+		fmt.Printf("%v %v\n", mouseX, mouseY)
+
+		// Создание изображения с красным пикселем в координатах mouseX, mouseY
+		redImage := ebiten.NewImage(1, 1)
+		redImage.Fill(color.RGBA{255, 0, 0, 255}) // Красный цвет
+
+		// Преобразование координат из окна в координаты изображения
+		opts := &ebiten.DrawImageOptions{}
+		opts.GeoM.Translate(float64(mouseX), float64(mouseY))
+
+		// Добавление изображения и его опций в соответствующие списки для отображения
+		g.images = append(g.images, redImage)
+		g.options = append(g.options, opts)
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	for i, img := range g.images {
+		opts := g.options[i]
+		screen.DrawImage(img, opts)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
